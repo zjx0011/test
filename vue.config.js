961f3d8357acf8addad4c98f18365bfbdff99f0b
zjx0011 <1206871986@qq.com>
2021-01-23 18:43:27 +0800

    接口代理

---
+++ vue.config.js
@@ -0,0 +1,13 @@
+module.exports = {
+    devServer: {
+        host: 'localhost',
+        port: 8080,
+        proxy: {
+            '/api': {
+                target: '',
+                changeOrigin: false
+            }
+        }
+
+    }
+}
