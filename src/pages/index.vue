37bab1edfb6cf55e79a4fac4750bda41a17df227
zjx0011 <1206871986@qq.com>
2021-01-28 19:51:55 +0800

    主界面

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -70,7 +70,35 @@
           <img src="/imgs/banner-1.png" alt="" />
         </a>
       </div>
-      <div class="product-box"></div>
+      <div class="product-box">
+        <h2>手机</h2>
+        <div class="wrapper">
+          <div class="banner-left">
+            <a href="/#/product/35">
+              <img src="/imgs/mix-alpha.jpg" alt="" />
+            </a>
+          </div>
+          <div class="list-box">
+            <div class="list" v-for="(arr, i) in phoneList" :key="i">
+              <div class="item" v-for="(item, j) in arr" :key="j">
+                <span>新品</span>
+                <div class="item-img">
+                  <img
+                    src="http://cdn.cnbj1.fds.api.mi-img.com/mi-mall/5a260090e0e08770b0bd865845a4b4ab.jpg?thumb=1&amp;w=313&amp;h=313&amp;f=webp&amp"
+                    alt=""
+                  />
+                </div>
+                <div class="item-info">
+                  <h3>小米9</h3>
+                  <p>骁龙855，索尼4800万超广角微距</p>
+                  <p class="price">2999元</p>
+                </div>
+              </div>
+            </div>
+            <div class="list"></div>
+          </div>
+        </div>
+      </div>
     </div>
     <service-bar></service-bar>
   </div>
@@ -174,6 +202,10 @@ export default {
           img: "/imgs/ads/ads-4.jpg",
         },
       ],
+      phoneList: [
+        [1, 1, 1, 1],
+        [1, 1, 1, 1],
+      ],
     };
   },
 };
@@ -271,6 +303,74 @@ export default {
   }
   .banner {
     margin-bottom: 50px;
+  }
+  .product-box {
+    background-color: $colorJ;
+    padding: 30px 0 50px;
+    h2 {
+      font-size: 22px;
+      height: 21px;
+      line-height: 21px;
+      color: $colorB;
+    }
+    .wrapper {
+      display: flex;
+      .banner-left {
+        margin-left: 16px;
+        img {
+          width: 224px;
+          height: 619px;
+        }
+      }
+      .list-box {
+        .list {
+          @include flex();
+          width: 986px;
+          margin-bottom: 14px;
+          &:last-child {
+            margin-bottom: 0;
+          }
+          .item {
+            width: 236px;
+            height: 302px;
+            background-color: $colorG;
+            text-align: center;
+            span {
+            }
+          }
+          .item-img {
+            img {
+              height: 195px;
+            }
+          }
+          .item-info {
+            h3 {
+              font-size: 14px;
+              color: $colorB;
+              line-height: 14px;
+              font-weight: bold;
+            }
+            p {
+              color: $colorD;
+              line-height: 13px;
+              margin: 6px auto 13px;
+            }
+            .price {
+              color: #f20a0a;
+              font-size: 14px;
+              font-weight: bold;
+              cursor: pointer;
+              &:after {
+                @include bgImg(22px.22px, "/imgs/icon-cart-hover.png");
+                content: "";
+                margin-left: 5px;
+                vertical-align: middle;
+              }
+            }
+          }
+        }
+      }
+    }
   }
 }
 </style>
